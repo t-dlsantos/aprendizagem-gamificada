78f73d5f3569fe7f439b1c48a08f81cce49e87ab
Thiago Santos <thiagodlsantos9@gmail.com>
2025-09-21 16:28:43 -0300

    feat: add h2 database

---
+++ src/test/java/com/grupoenzo/aprendizagem_gamificada/AprendizagemGamificadaApplicationTest.java
@@ -0,0 +1,18 @@
+package com.grupoenzo.aprendizagem_gamificada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AprendizagemGamificadaApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void shouldRunMain() {
+        AprendizagemGamificadaApplication.main(new String[0]);
+    }
+
+}
